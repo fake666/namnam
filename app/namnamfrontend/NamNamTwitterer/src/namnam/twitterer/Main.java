67c9727e46139d121c869e5c2f9b125bdca0d494
Thomas 'fake' Jakobi <thomas@derfake.com>
2009-05-28 13:01:23 +0000

    checkin diffs

--- app/namnamfrontend/NamNamTwitterer/src/namnam/twitterer/Main.java
+++ app/namnamfrontend/NamNamTwitterer/src/namnam/twitterer/Main.java
@@ -2,9 +2,11 @@ package namnam.twitterer;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.Locale;
 import java.util.Map;
 import namnam.parser.NamNamParser;
 import namnam.parser.erlangennuernberg.NamNamParserIN;
@@ -17,6 +19,7 @@ public class Main {
 
     private static NamNamParser myParser;
     private static DecimalFormat nf;
+    private static SimpleDateFormat df;
 
     private static final boolean doTwitter = false;
 
@@ -28,9 +31,9 @@ public class Main {
         myParser = new NamNamParserIN();
         nf = new DecimalFormat();
         nf.setMinimumFractionDigits(2);
-        DecimalFormatSymbols foo = new DecimalFormatSymbols();
-        foo.setDecimalSeparator(',');
+        DecimalFormatSymbols foo = new DecimalFormatSymbols(Locale.GERMAN);
         nf.setDecimalFormatSymbols(foo);
+        df = new SimpleDateFormat("E dd.MM.", Locale.GERMAN);
 
         Twitter twitter = new Twitter("hsin_mensa","toppas123");
 
@@ -38,6 +41,7 @@ public class Main {
             Map<Date,Map<String,Integer[]>> result = myParser.getCurrentMenues();
 
             Calendar now = Calendar.getInstance();
+            now.add(Calendar.DAY_OF_MONTH, 6);
             now.set(Calendar.HOUR_OF_DAY, 0);
             now.set(Calendar.MINUTE, 0);
             now.set(Calendar.SECOND, 0);
@@ -50,9 +54,10 @@ public class Main {
                 Integer[] preis = heid.get(desc);
                 // 0 = stud
                 // 1 = bed
+                desc = df.format(now.getTime()) + ": " +desc;
 
-                String up = ", " + nf.format(preis[0]/100.0) +
-                        " bzw. " + nf.format(preis[1]/100.0) + " EUR" ;
+                String up = " (" + nf.format(preis[0]/100.0) +
+                        " €/" + nf.format(preis[1]/100.0) + " €)" ;
 
                 if((up.length() + desc.length()) > 140) {
                     int toomuch = 140 - (up.length() + desc.length());
@@ -62,12 +67,13 @@ public class Main {
                 }
 
                 if(doTwitter)
-                    twitter.updateStatus(new String(up.getBytes("ISO-8859-15"),"ISO-8859-15"));
+                    twitter.updateStatus(up);
                 
-                System.out.println(new String(up.getBytes("ISO-8859-15"),"ISO-8859-15"));
+                System.out.println(up);
             }
         } catch (Exception ex) {
             System.err.println("Error twittering menues for today: " + ex);
+            ex.printStackTrace();
             if(doTwitter)
                 twitter.updateStatus("Sorry, es gibt ein Problem beim Abruf des Menues!");
         }
---
+++ app/namnamparser/NamNamParser/src/namnam/parser/erlangennuernberg/NamNamParserIN.java
@@ -0,0 +1,15 @@
+package namnam.parser.erlangennuernberg;
+
+
+/**
+ * parser specific for the ingolstadt URL
+ *
+ * @author fake
+ */
+public class NamNamParserIN extends NamNamParserErlangenNuernbergBase {
+
+    public NamNamParserIN() {
+        super();
+        this.theURL = "http://www.studentenwerk.uni-erlangen.de/verpflegung/de/sp-ingolstadt.shtml";
+    }
+}
